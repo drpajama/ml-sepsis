f6844e272ef1980b218dba0f9806c98cf0088b20
jpark <drpajama@gmail.com>
2016-07-07 04:01:04 -0400

    worked a bit

--- EchoKit.py
+++ EchoKit.py
@@ -2,10 +2,16 @@
 import psycopg2
 from VisitOccurence import ICUVisit
 import CareSite
+from datetime import timedelta
+from datetime import datetime
+from datetime import date
 
 SINGLE_VISIT, VISIT, PATIENTS, VISITS = range(4)
 
 class Echo:
+
+    talking = True
+
     care_sites = None
     focus = None
     visits_interested = None
@@ -18,10 +24,23 @@ class Echo:
         self.care_sites = self.get_caresites()
         self.focus = Focus()
 
-    def hi_echo(self):
-        print("\n-------")
-        print("You: Hi Echo!")
-        print("Echo: Hi Echo!\n")
+    def hello_echo(self):
+
+        if ( self.talking == False ):
+            print("\n-------")
+            print('You: "Hello Echo!"')
+            print("Echo: ...... \n")
+        else:
+            print("\n-------")
+            print('"You: Hello Echo!"')
+            print('"Echo: Hello Echo!"\n')
+
+    def shutup(self):
+        print('Echo: Okay. No more talking.')
+        self.talking = False
+
+        if (self.focus != None):
+            self.focus.talking = False
 
     def excuteSQL(self, query):
         cur = self.db_connect.cursor()
@@ -89,9 +108,40 @@ class Echo:
     def set_focus(self, target):
         self.focus.set_focus(target)
 
+    def ask_if_patient_died_focused_period(self):
+        if (self.focus.type == SINGLE_VISIT):
+            visit = self.focus.target
+            subject_id = visit.person_id
+
+            data = self.excuteSQL("SELECT * from ohdsi.death WHERE person_id = " + str(subject_id))
+            if (len(data) == 0):
+                if (self.talking == True):
+                    print ('\nEcho: "We have no record that the patient died in the hospital."\n')
+            else:
+                death_date = data[0][1]
+                death_datetime = datetime(year=death_date.year, day=death_date.day, month=death_date.month )
+                start_datetime = self.focus.start_datetime
+                end_datetime = self.focus.end_datetime
+
+                if (start_datetime <= death_datetime and death_datetime <= end_datetime):
+                    if (self.talking == True):
+                        print (
+                    '\nEcho: "We have a record that the patient died in the hospital during the period as the patient died on ' + str(
+                        death_date) + ', which is between ' + str(start_datetime) + ' and ' + str(
+                        end_datetime) + '.\n')
+                    return (True, dict(death_date=death_date))
+                else:
+                    if (self.talking == True):
+                        print ('\nEcho: "We have a record that the patient died in the hospital, but it was not during the period specified. The patient died on ' + str(
+                            death_date) + ', which IS NOT between ' + str(start_datetime) + ' and ' + str(
+                        end_datetime) + '.\n')
+                    return (False, dict(death_date=death_date))
+
+
     def ask_if_patient_died_during_the_visit(self):
         if (self.focus == None or self.focus.target == None):
-            print ('Echo: "You said you want to know whether a patient died? but.... which patient(s)?"')
+            if (self.talking == True):
+                print ('\nEcho: "You said you want to know whether a patient died? but.... which patient(s)?"')
             return None
 
         if (self.focus.type == SINGLE_VISIT):
@@ -100,7 +150,8 @@ class Echo:
 
             data = self.excuteSQL("SELECT * from ohdsi.death WHERE person_id = " + str(subject_id))
             if ( len(data) == 0 ):
-                print ('\nEcho: "We have no record that the patient died in the hospital."\n')
+                if (self.talking == True):
+                    print ('\nEcho: "We have no record that the patient died in the hospital."\n')
                 return (False, dict())
 
             else:
@@ -109,10 +160,12 @@ class Echo:
                 visit_end_date = visit.end_date
 
                 if ( visit_start_date <= death_date and death_date <= visit_end_date  ):
-                    print ('\nEcho: "We have a record that the patient died in the hospital during the visit. The patient died on ' + str(death_date) + ', which is between ' + str(visit_start_date) + ' and ' + str(visit_end_date) + ' (the duration of the target ICU visit.)\n')
+                    if (self.talking == True):
+                        print ('\nEcho: "We have a record that the patient died in the hospital during the visit. The patient died on ' + str(death_date) + ', which is between ' + str(visit_start_date) + ' and ' + str(visit_end_date) + ' (the duration of the target ICU visit.)\n')
                     return (True, dict(death_date=death_date))
                 else:
-                    print (
+                    if (self.talking == True):
+                        print (
                     '\nEcho: "We have a record that the patient died in the hospital, but it was not during the visit. The patient died on ' + str(
                         death_date) + ', which IS NOT between ' + str(visit_start_date) + ' and ' + str(
                         visit_end_date) + ' (the duration of the target ICU visit.)\n')
@@ -127,9 +180,11 @@ class Focus:
     visit = None
     type = None
     time_point = None
-    start_time = None
-    end_time = None
+    duration = None
+    start_datetime = None
+    end_datetime = None
     target = None
+    talking = True
 
     def __init__(self):
         return
@@ -138,19 +193,83 @@ class Focus:
         self.visit = None
         self.type = None
         self.time_point = None
-        self.start_time = None
-        self.end_time = None
+        self.start_datetime = None
+        self.end_datetime = None
 
         self.target = target
 
         if ( isinstance(target, ICUVisit) ):
             self.type = SINGLE_VISIT
-            print('\nEcho: "Okay, I will focus on the visit of the patient_id: ' + str(target.person_id) + ', who visited ICU on ' + str(target.start_date) + '."\n')
+            if (self.talking == True):
+                print('\nEcho: "Okay, I will focus on the visit of the patient_id: ' + str(target.person_id) + ', who visited ICU on ' + str(target.start_date) + '."\n')
 
 
+    def set_time_point(self, timepoint, duration = timedelta(hours=3) ):
+
+        self.duration = duration
+        if ( isinstance(timepoint, datetime) ):
+            self.time_point = timepoint
+        else :
+            raise ValueError('Error: The time point for focus should be the type of datetime.datetime.')
+
+        self.start_datetime = self.time_point - duration
+        self.end_datetime = self.time_point + duration
+
+        if (self.talking == True):
+            print('Echo: "I understand that you are intrested in what happend at ' + str(self.time_point) + " during the hospitalization. (plus/minus " + str(self.duration) + " hours) Therefore, it will be between " + str(self.start_datetime) + " and " + str(self.end_datetime) + ". ")
 
 
+    def set_date_point(self, datepoint, duration = timedelta(hours=24)):
+        self.start_datetime = datetime(year=datepoint.year, month=datepoint.month, day=datepoint.day, hour=0, minute=0, second=0)
+        self.duration = duration
+        self.end_datetime = self.start_datetime + duration
 
+        if (self.talking == True):
+            print('Echo: "I understand that you are intrested in what happend at ' + str(self.start_datetime) + " during the hospitalization. (will assume " + str(self.duration) + " period) Therefore, it will be between " + str(self.start_datetime) + " and " + str(self.end_datetime) + ".")
+
+    def set_start_end_datetime(self, start_datetime, end_datetime):
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.duration = end_datetime - start_datetime
+        if (self.talking == True):
+            print('Echo: "I understand that you are intrested in what happend at ' + str(
+            self.start_datetime) + " during the hospitalization. (will be " + str(
+            self.duration) + " period) Therefore, it will be between " + str(self.start_datetime) + " and " + str(
+            self.end_datetime) + ".")
+
+        return
+
+    def set_start_end_date(self, start_date, end_date):
+        self.start_datetime = datetime(year=start_date.year, month=start_date.month, day=start_date.day, hour=0, minute=0,
+                                   second=0)
+        self.end_datetime = datetime(year=end_date.year, month=end_date.month, day=end_date.day, hour=0, minute=0,
+                                   second=0)
+
+        self.duration = self.end_datetime - self.start_datetime
+        return
+
+
+    def time_forward(self, duration):
+        self.start_datetime = self.start_datetime + duration
+        self.end_datetime = self.end_datetime + duration
+        if (self.time_point != None):
+            self.time_point = self.time_point + duration
+
+    def days_later(self, days):
+        self.time_forward(  timedelta( days = days)  )
+        if (self.talking == True):
+            print('Echo: "I understand that now you are intrested in what happend at ' + str(
+            self.start_datetime) + " during the hospitalization. (will be " + str(
+            self.duration) + " period) Therefore, it will be between " + str(self.start_datetime) + " and " + str(
+            self.end_datetime) + ".")
+
+    def hours_later(self, hours):
+        self.time_forward(timedelta(hours=hours))
+        if (self.talking == True):
+            print('Echo: "I understand that now you are intrested in what happend at ' + str(
+            self.start_datetime) + " during the hospitalization. (will be " + str(
+            self.duration) + " period) Therefore, it will be between " + str(self.start_datetime) + " and " + str(
+            self.end_datetime) + ".")
 
 
 '''
@@ -185,7 +304,7 @@ class Loader:
         print("Connecting to database -> %s " % (conn_string))
 
 
-    def get_db_connection(self):
+    def get_connection(self):
         return self.conn
 
 
--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import EchoKit
 
 loader = EchoKit.Loader( dbname = "mimic2", user = "jpark", password = "pc386pc386" )
 
-echo = EchoKit.Echo( loader.get_db_connection() )
-echo.hi_echo()
+echo = EchoKit.Echo( loader.get_connection() )
+echo.hello_echo()
 
 visits = echo.get_random_visits(2)
 echo.set_focus( visits[0] )
-echo.ask_if_patient_died_during_the_visit()
+echo.focus.set_time_point( timepoint = visits[0].get_start_datetime()  )
+echo.focus.set_start_end_datetime( start_datetime = visits[0].get_start_datetime(), end_datetime = visits[0].get_end_datetime() )
+echo.focus.hours_later(7)
+echo.focus.days_later(2)
 
+echo.ask_if_patient_died_during_the_visit()
+echo.ask_if_patient_died_focused_period()
+#echo.ask_if_patient_died_during_the_focused_period()
 
 for visit in visits:
     visit.description()
